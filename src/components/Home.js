9c5d0525278d09ba31b35436c0bf3deb34306c80
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-10-21 10:25:55 -0300

    Navigation made

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import SearchComponent from './searchComponent';
+import NavigationBar from './navigationBar';
+import NotificationCard from './notificationCard';
+import { commonStyles } from './styles';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={commonStyles.container}>
+      <SearchComponent />
+      <ScrollView contentContainerStyle={commonStyles.scrollViewContent}>
+        <View style={commonStyles.content}>
+          {/* Contenido específico de la pestaña de Plantas */}
+          <NotificationCard />
+        </View>
+      </ScrollView>
+      <View style={commonStyles.shadowContainer}>
+        <View style={commonStyles.topLine}></View>
+        <View style={commonStyles.bottomContainer}>
+          <NavigationBar name="Home" icon="home-variant" text="Home" active={true} navigation={navigation} />
+          <NavigationBar name="Plants" icon="sprout" text="Plants" navigation={navigation} />
+          <NavigationBar name="Calendar" icon="calendar" text="Calendar" navigation={navigation} />
+          <NavigationBar name="Settings" icon="cog" text="Settings" navigation={navigation} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default HomeScreen;
