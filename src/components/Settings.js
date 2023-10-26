82a7fd4d938c48604dbab3be9722fa07e2a7aa1d
Faqu22 <facundoalvarez415@gmail.com>
2023-10-26 15:26:54 -0300

    Implementation of bluetooth

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,127 +1,109 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, ScrollView, Button, FlatList, Text, PermissionsAndroid, Alert } from 'react-native';
+import { View, Button, FlatList, Text, TextInput, Alert } from 'react-native';
 import NavigationBar from './navigationBar';
 import { commonStyles } from './styles';
-import RNBluetoothClassic from 'react-native-bluetooth-classic'
-import { PERMISSIONS, request } from 'react-native-permissions';
+import {
+    sendMessage,
+    connectToDevice,
+    fetchPairDevices,
+    bluetoothEnabled,
+    discoverDevices
+  } from './BlueetoothLogic';
+
+  const SettingsScreen = ({ navigation }) => {
+    const [responseWifi, setResponseWifi] = useState('');
+    const [responseUuid, setResponseUuid] = useState('');
+    const [devices, setDevices] = useState([]);
+    const [connectedDevice, setConnectedDevice] = useState(null);
+    const [wifiName, setWifiName] = useState('');
+    const [wifiPassword, setWifiPassword] = useState('');
+    const [availableDevices, setAvailableDevices] = useState([]);
 
 
-const SettingsScreen = ({ navigation }) => {
-  const [response, setResponse] = useState('');
-  const [devices, setDevices] = useState([]);
-  const [connectedDevice, setConnectedDevice] = useState(null);
-
-
-  async function requestBluetoothPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("Tienes acceso a Bluetooth");
-      } else {
-        console.log("Permiso de Bluetooth denegado");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
-
-  const sendMessage = async (txt) => {
-    if (!connectedDevice) {
-      return;
-    }
-    try {
-      const success = await RNBluetoothClassic.writeToDevice(
-        connectedDevice.address,
-        txt
-      );
-      console.log(success);
-      if (success) {
-        console.log("TRY")
-        await sleep(10000)
-        const response = await RNBluetoothClassic.readFromDevice(
-          connectedDevice.address
-        );
-        setResponse(response);
-        console.log(response)
-      } else {
-        Alert.alert('Error al enviar mensaje');
-      }
-    } catch (error) {
-      console.error('Error sending message:', error);
-    }
-  };
-  
-  const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  const connectToDevice = async (device) => {
-    try {
-      const connection = await RNBluetoothClassic.connectToDevice(device.address);
-      if (connection) {
-        console.log("test-\n", connection);
-        setConnectedDevice(device);
-        console.log("Finish")
-      }
-    } catch (error) {
-      console.error('error', error);
-    }
-  };
-
-  const sendwifi = async () => {
-    const message1 = JSON.stringify({status_code:1,data:{wifi:"planty",password:"12345678"}});
-    sendMessage(message1)
-  }
-
-  const getuuid = async () => {
-    const message2 = JSON.stringify({status_code:2});
-    await sendMessage(message2)
-    Alert.alert(response)
-  }
-
-  useEffect(() => {
-    async function fetchPairDevices() {
-      const pairDevices = await RNBluetoothClassic.getBondedDevices();
-      const filterList = [];
-      for (const device of pairDevices) {
-        if (device.name.startsWith("Planty-", 0)) {
-          filterList.push(device);
+    const sendwifi = async (device) => {
+        const wifi = wifiName || "zunzun07-2.4GHz"; // to exists wifi and password default.
+        const password = wifiPassword || "49861978"; 
+        const message1 = JSON.stringify({status_code:1, data:{ssid:wifi,password:password}});
+        const request = await sendMessage(device, message1);
+        if (request === "{status_code:6}"){
+            Alert.alert("Error de credenciales"); //Manejo de error al no poder conectar el arduino con el wifi
         }
-      }
-      setDevices(filterList)
+        else{
+            setResponseWifi(request);
+        }
     }
-    RNBluetoothClassic.requestBluetoothEnabled().then(enabled => {
-      if (enabled) {
-        fetchPairDevices();
-      }
-    })
-      .catch(error => {
-        console.error(`Error habilitando Bluetooth: ${error.message}`);
-      });
-  }, []);
+  
+    const getuuid = async () => {
+      const message2 = JSON.stringify({status_code:2});
+      const request = await sendMessage(connectedDevice, message2);
+      setResponseUuid(request);
+    }
+  
+    useEffect(() => {
+        bluetoothEnabled().then(enabled => {
+            if (enabled) {
+                fetchPairDevices().then(filterList => {
+                    setDevices(filterList);
+                });
+            }
+        })
+        .catch(error => {
+          console.error(`Error habilitando Bluetooth: ${error.message}`);
+        });
+    }, []);
 
 return (
   <View style={commonStyles.container}>
-    <ScrollView contentContainerStyle={commonStyles.scrollViewContent}>
       <View style={commonStyles.content}>
-        <Text>Devices:</Text>
+      <FlatList
+          data={availableDevices}
+          keyExtractor={availableDevices => availableDevices.address}
+          renderItem={({ availableDevices }) => (
+            <View>
+                <Button title="connect to " onPress={() => connectToDevice(availableDevices).then(device => {setConnectedDevice(device)})}/>
+                <Text> {availableDevices.name}</Text>
+            </View>
+          )}/>
         <FlatList
           data={devices}
           keyExtractor={item => item.address}
           renderItem={({ item }) => (
             <View>
-              <Text> {item.name} </Text>
-              <Button title='connect' onPress={() => connectToDevice(item)} style={{ margin: 10 }}/>
-              <Button title='wifi' onPress={() => sendwifi()} style={{ margin: 10 }}/>
-              <Button title='uuid' onPress={() => getuuid()} style={{ margin: 10 }}/>
+                <Text style={{ backgroundColor: connectedDevice ? "green" : "gray", marginBottom: 10}}> {item.name} </Text>
+                <Button title='connect' onPress={() => connectToDevice(item).then(device => {setConnectedDevice(device)})}/>
+                <Text style={{ marginTop: 10 }}>Nombre WiFi:</Text>
+                <TextInput
+                    style={{borderWidth: 1, borderColor: 'grey', marginBottom: 10, padding: 5}}
+                    placeholder="Introduce el nombre del WiFi"
+                    onChangeText={text => setWifiName(text)}
+                    defaultValue={wifiName}
+                />
+                <Text>Contraseña WiFi:</Text>
+                <TextInput
+                    style={{borderWidth: 1, borderColor: 'grey', marginBottom: 10, padding: 5}}
+                    placeholder="Introduce la contraseña del WiFi"
+                    onChangeText={text => setWifiPassword(text)}
+                    defaultValue={wifiPassword}
+                    secureTextEntry={true}
+                />
+                {/* se pasa el dispositivo para tenerlo en contexto */}
+                <View style={{ marginVertical: 10}}>
+                 {responseWifi && (
+                    <Text> Se conecto: {responseWifi} </Text>
+                 )}   
+                <Button title='Enviar WiFi' onPress={() => sendwifi(item)}/> 
+                </View>
+                <View style={{ marginVertical: 15}}>
+                    {responseUuid && (
+                    <Text> La uuid de la planty es: {responseUuid} </Text>
+                    )}
+                    <Button title='uuid' onPress={() => getuuid()}/>
+                </View>
 
             </View>
           )}
         />
       </View>
-    </ScrollView>
     <View style={commonStyles.shadowContainer}>
       <View style={commonStyles.topLine}></View>
       <View style={commonStyles.bottomContainer}>
