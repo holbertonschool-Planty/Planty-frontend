1dcbd109f7b172fd47c654a7173bbb53d37bda88
Faqu22 <facundoalvarez415@gmail.com>
2023-10-27 23:25:00 -0300

    Implementation of google sign in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,64 @@
-import React, { useState } from 'react';
-import { View, ScrollView, Modal, TouchableOpacity, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, Modal, TouchableOpacity, Text, Button } from 'react-native';
 import SearchComp from './SearchComp.js';
 import NavigationBar from './navigationBar';
 import NotificationCard from './notificationCard';
 import { commonStyles } from './styles';
+import { GoogleSignin, GoogleSigninButton,statusCodes} from '@react-native-google-signin/google-signin';
+
 
 const HomeScreen = ({ navigation }) => {
   const [showSearchResults, setShowSearchResults] = useState(false);
+  const [actualUser, SetActualUser] = useState();
+  const [infoUser, SetInfoUser] = useState();
+
+
+
+  getCurrentUser = async () => {
+    const currentUser = await GoogleSignin.getCurrentUser();
+    SetActualUser({ currentUser });
+    console.log("a", currentUser);
+  };
+
+  signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      SetInfoUser({ user: null }); // Remember to remove the user from your app's state as well
+      console.log("b")
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      SetInfoUser(userInfo);
+      console.log("A")
+      console.log(userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log("ERROR CANCELLED")
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log("ERROR INPROGRESS")
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log("ERROR SERVICES")
+    } else {
+        console.log("ERROR NIDEA\n", JSON.stringify(error, null, 2));
+        
+      }
+    }
+  };
+
+  useEffect(() => {
+    request = GoogleSignin.configure({
+        webClientId: "678386882303-r4tvvhp3cf5imko6qa9fvfuqv4u4l8c9.apps.googleusercontent.com"
+    });
+    service = GoogleSignin.hasPlayServices();
+    console.log(request);
+    console.log(service);
+}, []);
 
   return (
     <View style={commonStyles.container}>
@@ -14,6 +66,9 @@ const HomeScreen = ({ navigation }) => {
       <ScrollView contentContainerStyle={commonStyles.scrollViewContent}>
         <View style={commonStyles.content}>
           <Text style={commonStyles.homeActivity}>Activity</Text>
+            <Button title='Sign'  onPress={signIn}/>
+            <Button title='Unsign'  onPress={signOut}/>
+            <Button title='User?'  onPress={getCurrentUser}/>
           <NotificationCard />
         </View>
       </ScrollView>
