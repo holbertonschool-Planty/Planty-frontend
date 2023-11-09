3104ca5bc7dc5a14d5ff6636fe21224130b2ed6b
Faqu22 <facundoalvarez415@gmail.com>
2023-11-09 20:44:12 -0300

    Upgrade Loading Screen

--- planty-app/src/components/LoadingScreen.js
+++ planty-app/src/components/LoadingScreen.js
@@ -32,7 +32,7 @@ const LoadingScreen = ({ navigation }) => {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-      <Animatable.View animation="zoomIn" duration={3000} style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Animatable.View animation="zoomIn" duration={2000} style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
         <Image source={require('../img/Logo_App_Planty.png')} style={{ height: 300 }} resizeMode="contain" />
       </Animatable.View>
     </View>
